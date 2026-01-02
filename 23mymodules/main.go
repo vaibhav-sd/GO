b5f13d7c5b0186178720a96078f61bd86c7f462a
Vaibhav Dagwal <vaibhav.dagwal2015@gmail.com>
2026-01-02 15:21:40 +0530

    mod in GO

---
+++ 23mymodules/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	fmt.Println("MOD in GO")
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":4000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to GO</h1>"))
+}
